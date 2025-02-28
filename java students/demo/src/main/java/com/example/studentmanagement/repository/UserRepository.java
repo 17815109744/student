729dfc22c04b4c42617647fbbe926f13c13f7715
17815109744 <145094781+17815109744@users.noreply.github.com>
2025-03-01 01:55:25 +0800

    Delete java students/demo directory

--- java students/demo/src/main/java/com/example/studentmanagement/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
