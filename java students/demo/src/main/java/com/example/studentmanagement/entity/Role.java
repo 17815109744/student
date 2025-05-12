23f694bb7769277497579960dbe880ddae12a5e1
17815109744 <145094781+17815109744@users.noreply.github.com>
2025-05-13 07:08:33 +0800

    Delete java students/demo directory

--- java students/demo/src/main/java/com/example/studentmanagement/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagement.entity;
-
-public enum Role {
-    ADMIN,
-    TEACHER,
-    STUDENT
-}
--- java students/demo/src/main/java/com/example/studentmanagement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByCourses_Id(Long id);
-}
